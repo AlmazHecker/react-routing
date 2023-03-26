af9b35533ab68c1e113981734dc3ae6aec888339
AlmazHecker <89141838+AlmazHecker@users.noreply.github.com>
2023-03-26 17:45:37 +0600

    optimize useParams hook

--- src/hooks/useParams.ts
+++ src/hooks/useParams.ts
@@ -4,17 +4,20 @@ import { SearchParam } from './useSearchParams';
 
 const useParams = (path: string): SearchParam => {
   const { state } = useRouterContext();
-  const params: SearchParam = {};
-  const parts = path.split('/');
 
-  for (let i = 0; i < parts.length; i++) {
-    if (parts[i].startsWith(':')) {
-      const paramName = parts[i].substring(1);
-      params[paramName] = parts[i + 1];
+  const values = useMemo(() => {
+    const params: SearchParam = {};
+    const parts = path.split('/');
+
+    for (let i = 0; i < parts.length; i++) {
+      if (parts[i].startsWith(':')) {
+        const paramName = parts[i].substring(1);
+        params[paramName] = parts[i + 1];
+      }
     }
-  }
+    return params;
+  }, [state.location]);
 
-  const values = useMemo(() => params, [state.location]);
   return values;
 };
 
